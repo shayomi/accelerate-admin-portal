82a0ae60d67bb5d733878e137a81af0786f52733
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-17 17:55:44 +0100

    Update types.ts

--- pages/types.ts
+++ pages/types.ts
@@ -1,43 +1,41 @@
 import React from "react";
 
-
 export type Founder = {
-    name: string;
-    role: string;
-    linkedin: string;
-    image: string;
+  name: string;
+  role: string;
+  linkedin: string;
+  image: string;
+};
+
+export type Startup = {
+  id: string;
+  companyName: string;
+  industry: string;
+  cohort: string;
+  founders: Founder[];
+  description: string;
+  pitchVideoLink: string;
+  companyLogo: string;
+  companyEmail: string;
+  website: string;
+  location: {
+    city: string;
+    country: string;
   };
-  
-  export type Startup = {
-    id: string;
-    companyName: string;
-    industry: string;
-    cohort: string;
-    founders: Founder[];
-    description: string;
-    pitchVideoLink: string;
-    companyLogo: string;
-    companyEmail: string;
-    website: string;
-    location: {
-      city: string;
-      country: string;
-    };
-    companySize: number;
-    fundraisingAmount: number;
-    valuation: number;
-    industries: string[];
-    founded: number;
-    pitchDeckLink: string;
-    calendlyLink: string;
-    status: string;
-  };
+  companySize: number;
+  fundraisingAmount: number;
+  valuation: number;
+  industries: string[];
+  founded: number;
+  pitchDeckLink: string;
+  calendlyLink: string;
+  status: string;
+};
 
 // types.ts
 export type Industry = {
-  id: number; 
+  id: number;
   name: string;
   slug?: string;
-  startupIds: number[]; 
+  startupIds: number[];
 };
-
