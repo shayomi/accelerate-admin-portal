b6a7a11799ce4cde3361967c9d5eada03f818dde
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-18 14:02:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /**@type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
- 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
