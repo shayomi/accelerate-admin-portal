bd1ac9a7b64f20aaeade42342d3ac2c9ac482242
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-18 16:20:40 +0100

    b

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
+/* eslint-disable indent */
 /**@type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-	output: "export",
-	reactStrictMode: true,
-	trailingSlash: true,
-	swcMinify: true,
-	basePath: "",
-	assetPrefix: "",
-	images: {
-		loader: "imgix",
-		path: "/",
-	},
+  output: "export",
+  reactStrictMode: true,
+  trailingSlash: true,
+  swcMinify: true,
+  basePath: "",
+  assetPrefix: "",
+  images: {
+    loader: "imgix",
+    path: "/",
+  },
 };
 
 module.exports = nextConfig;
