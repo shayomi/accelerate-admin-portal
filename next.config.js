1c5ec7b171720695e872e8e9c514f9d40fdb78d3
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-18 15:08:00 +0100

    n

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
 /**@type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  output: 'export',
-  reactStrictMode: true,
-  trailingSlash: true,
-  swcMinify: true,
-  basePath: '',
-  assetPrefix: '',
-  images: {
-    loader: 'imgix',
-    path: '/',
-  },
+	output: "export",
+	reactStrictMode: true,
+	trailingSlash: true,
+	swcMinify: true,
+	basePath: "",
+	assetPrefix: "",
+	images: {
+		loader: "imgix",
+		path: "/",
+	},
 };
 
 module.exports = nextConfig;
