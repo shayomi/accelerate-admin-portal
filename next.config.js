5784a60c640eef990a1362db0bbb738319c9af8f
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-19 05:01:06 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 /**@type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
+  distDir: "build",
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
