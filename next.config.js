2e435095e7b84ad90203d18af13be0859dc0375f
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-19 04:45:20 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 /**@type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
