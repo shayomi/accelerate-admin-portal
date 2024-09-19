2300d31c1a945de98319b3f6c4aca320cca5c176
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-19 04:36:35 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "../styles/globals.scss";
 import ContentLayout from "../shared/layout-components/layout/content-layout";
 import Authenticationlayout from "../shared/layout-components/layout/authentication-layout";
 import Landinglayout from "@/shared/layout-components/layout/landing-layout";
+import React from "react";
 
 const layouts: any = {
-  Contentlayout: ContentLayout,
-  Landinglayout: Landinglayout,
-  Authenticationlayout: Authenticationlayout,
+	Contentlayout: ContentLayout,
+	Landinglayout: Landinglayout,
+	Authenticationlayout: Authenticationlayout,
 };
 function MyApp({ Component, pageProps }: any) {
-  const Layout =
+	const Layout =
     layouts[Component.layout] ||
     ((pageProps: any) => <Component>{pageProps}</Component>);
 
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	);
 }
 
 export default MyApp;
