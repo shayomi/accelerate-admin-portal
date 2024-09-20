a23595d627c00f4008c01bd63756547f8e1fd237
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-20 15:31:00 +0100

    new

--- next/server/middleware-build-manifest.js
+++ next/server/middleware-build-manifest.js
@@ -19,6 +19,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
+    "/dashboard": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/dashboard.js"
+    ],
     "/events": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
--- next/static/chunks/webpack.js
+++ next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ebce69b474d05395"; }
+/******/ 		__webpack_require__.h = function() { return "5fc94103ee8288b3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
