53377dd0b6cfb63d05f1b5ba75aaac8789679a4a
Sayo Adegoroye <steve.legion94@gmail.com>
2024-09-20 15:04:50 +0100

    new

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 /* eslint-disable indent */
 /**@type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  distDir: "build",
+  distDir: "next",
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
